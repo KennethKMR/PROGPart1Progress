7f3d9b34e0a28504fbda5bd8546ae5cb00528913
Kenneth Michael Remmitz <st10403075@rcconnect.edu.za>
2024-04-11 09:08:40 +0200

    Add project files.

---
+++ PROG6221Part1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace PROG6221Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recipe recipe = new Recipe();
+            Console.WriteLine("Enter the name of the recipe: ");
+            recipe.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter the number of ingredients: ");
+            int numIngredients = int.Parse(Console.ReadLine());
+        }
+    }
+}
